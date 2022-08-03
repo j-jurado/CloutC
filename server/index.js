b74a5c18f1a0d31eaca5466af7d66672870e18fd
j-jurado <73544267+j-jurado@users.noreply.github.com>
2022-08-02 22:31:52 -0400

    user profile fetch functions

--- server/index.js
+++ server/index.js
@@ -11,9 +11,6 @@ app.listen(3001, () => {
     console.log("SERVER RUNNING SUCCESSFULLY");
 });
 
-// npm install twitter
-// npm install dotenv
-
 // Apply for developer key for access to twitter API.
 // create an .env file and store variables like: apikey = 33453@443
 // in the file you want to use them call variables like: apikey = process.env.apikey
@@ -22,12 +19,13 @@ app.listen(3001, () => {
 //https://www.youtube.com/watch?v=Zsu3R0IOHps @4:53
 
 
-//API Credentials
+//API credentials
 const apiKey = process.env.apikey;
 const apiSecreteKey = process.env.apikeysecret;
 const accessToken = process.env.accesstoken;
 const accessTokenSecret = process.env.accesstokensecret;
 
+//Twitter access client
 var client = new Twitter({
     consumer_key: apiKey,
     consumer_secret: apiSecreteKey,
@@ -35,12 +33,36 @@ var client = new Twitter({
     access_token_secret:  accessTokenSecret
 });
 
-//Example of API get request
-var params = {screen_name: 'Jae78613139'};
-client.get('statuses/user_timeline', params, function(error, tweets, response) {
-    if(!error) {
-        console.log(tweets);
-    } else {
-        console.log(error);
-    }
-});
+//(DELETE LATER) Test user profile fetch
+function getUserProfileInfo(scr_name) {
+    var params = {screen_name: scr_name};
+    client.get('users/show', params, function(error, info, response) {
+        if(!error) {
+            console.log("SHOWING USER INFORMATION...");
+            console.log(info);
+        } else {
+            console.log(error);
+        }
+    });
+}
+getUserProfileInfo("jesusjrdo");
+
+//User profile fetch html compatible
+function getUserProInfo(req, res) {
+    var params = {screen_name: req.body};
+    client.get('users/show', params, function(error, info, response) {
+        if(!error) {
+            console.log(info);
+            res.json(info);
+        } else {
+            console.log(error);
+        }
+    });
+}
+
+//TODO: create front end textbox to call this function
+//Look at:
+//https://github.com/imar26/twitter-webapp/blob/master/server/app.js
+//https://github.com/rgarber11/Dosca/blob/main/dosca-whiteboard/server/server.js
+//https://github.com/machadop1407/MERN-Beginners/blob/main/server/index.js
+
