e751c7cc56a8f76e0382bfd7c32b837cfb8f0d4d
MarielaB118 <MarielaB118@users.noreply.github.com>
2022-08-07 22:33:13 -0400

    score page

--- client/src/Main.js
+++ client/src/Main.js
@@ -1,23 +1,10 @@
-
+/*
 const Main = () => {
     
     return (
-        <div className='White-Space'>
-            <h1>Screen Name</h1>
-            <h4>User_name</h4>
-            <h2>Most Liked Tweet</h2>
-            <h2>Profile Stats</h2>
-            <h2>Famous Followers</h2>
-            <h3>Clout Score</h3>
-            <h5>Above Average</h5>
-            <h5>Average Likes</h5>
-            <h5>Retweet Count</h5>
-            <h5>Tweet Count</h5>
-            <p>Paragraph in main page</p>
-            <ellipse/>
-        </div> 
+        
 
      );
 }
  
-export default Main;
+export default Main;*/
--- client/src/components/Score.js
+++ client/src/components/Score.js
@@ -1,8 +1,30 @@
 import './Score.css';
-
+import AnimatedNumber from 'react-animated-number'
 function Score (){
     return(
-        <h1>Score Page</h1>
+        <div className='background'>
+            <div className='White-Space'>
+                <h1 id='screen-name'>Screen Name</h1>
+                <h4 id='user-name'>User_name</h4>
+                <h2>Most Liked Tweet</h2>
+                <h2>Profile Stats</h2>
+                <h2>Famous Followers</h2>
+                <h3>Clout Score</h3>
+                <h5>Above Average</h5>
+                <h5>Average Likes</h5>
+                <h5>Retweet Count</h5>
+                <h5>Tweet Count</h5>
+                <p>Paragraph in main page</p>
+                <AnimatedNumber className="animated-number" value={40} 
+                formatValue={v => v.toFixed(0)} 
+                duration={1000} 
+                frameStyle={perc => (
+                    {opacity: perc / 100}
+                )} />
+                {/*<ellipse />*/}
+                <ellipse id='profile-pic' />
+            </div>
+        </div>
     )
 }
 export default Score;
